7379c529009401c0cf0985373698feec1926d96c
6110613269 <60641033+6110613269@users.noreply.github.com>
2021-08-26 21:55:35 +0700

    first commit

---
+++ Tic_tac_toe_pat/Tic tac toe pat/Tic tac toe pat/Tic_tac_toe_patApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_tac_toe_patApp.swift
+//  Tic tac toe pat
+//
+//  Created by Siriluk Rachaniyom on 26/8/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Tic_tac_toe_patApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
